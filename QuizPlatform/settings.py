1864a6f91692e3bc344ffa43174be6746a3c797b
Mehdi Hoseini <mh9170525@gmail.com>
2026-06-06 12:26:04 -0700

    Add whitenoise and fix static files

--- QuizPlatform/settings.py
+++ QuizPlatform/settings.py
@@ -119,7 +119,9 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = [
+    BASE_DIR / "static",  
     BASE_DIR / "quizzes" / "static", 
+    BASE_DIR / "accounts" / "static", 
 ]
 
 STATIC_ROOT = BASE_DIR / "staticfiles"
